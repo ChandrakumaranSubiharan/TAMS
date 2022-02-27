3137e4f62247f01f9d922a0ca77d8004fbeab37d
ChandrakumaranSubiharan <wordpress.subiharan@gmail.com>
2022-02-27 21:09:58 +0530

    index commit 01

---
+++ includes/header.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Meta Tags -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TAMS | HappyHolidayss</title>
+    <meta name="keywords" content="happyholidayss" />
+    <meta name="description" content="HappyHolidayss | Accomodations & Tours">
+    <meta name="author" content="Subiharan">
+
+    <!-- Theme Styles -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    
+    <!-- Current Page Styles -->
+    <link rel="stylesheet" type="text/css" href="assets/components/revolution_slider/css/settings.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="assets/components/revolution_slider/css/style.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="assets/components/jquery.bxslider/jquery.bxslider.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="assets/components/flexslider/flexslider.css" media="screen" />
+    
+    <!-- Main Style -->
+    <link id="main-style" rel="stylesheet" href="assets/css/components/header.css">
+    <link id="main-style" rel="stylesheet" href="assets/css/style.css">
+    
+    <!-- Responsive Styles -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    
+</head>
+    <body>
+            <header id="header" class="navbar-static-top">
+                <div class="topnav hidden-xs">
+                    <div class="container">
+                        <ul class="quick-menu pull-right">
+                            <li><a href="#travelo-login" class="soap-popupbox">LOGIN</a></li>
+                            <li><a href="#travelo-signup" class="soap-popupbox">SIGNUP</a></li>
+                            <li><a href="#travelo-signup" class="#">Become a Partner</a></li>
+                        </ul>
+                        <ul class="quick-menu pull-left">
+                            <li><a href="#travelo-login" class="soap-popupbox">Phone: +(094) 125-4985-214</a></li>
+                            <li><a href="#travelo-signup" class="soap-popupbox">Email: info@happyholidayss.com</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div class="main-header">
+                    
+                    <a href="#mobile-menu-01" data-toggle="collapse" class="mobile-menu-toggle">
+                        Mobile Menu Toggle
+                    </a>
+
+                    <div class="container">
+                        <h1 class=" logo-header navbar-brand">
+                            <a href="index.html" title="HappyHolidayss - home">
+                                <img src="assets/images/logo.svg" alt="HappyHolidayss_Logo" />
+                            </a>
+                        </h1>
+                        
+                        <nav id="main-menu" role="navigation">
+                            <ul class="menu">
+                                <li class="menu-item-has-children">
+                                    <a href="index.php">Home</a>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="hotel-index.html">Tours</a>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="car-index.html">Stays</a>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="cruise-index.html">About Us</a>
+                                </li>
+                                <li class="menu-item-has-children megamenu-menu">
+                                    <a href="#">Contact Us</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    
+                    <nav id="mobile-menu-01" class="mobile-menu collapse">
+                        <ul id="mobile-primary-menu" class="menu">
+                            <li>
+                                <a href="index.php">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">Tours</a>
+                            </li>
+                            <li>
+                                <a href="#">Stays</a>
+                            </li>
+                            <li>
+                                <a href="#">About Us</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                        
+                        <ul class="mobile-topnav container">
+                            <li><a href="#travelo-login" class="soap-popupbox">LOGIN</a></li>
+                            <li><a href="#travelo-signup" class="soap-popupbox">SIGNUP</a></li>
+                            <li><a href="#travelo-signup" >BECOME A PARTNER</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div id="travelo-signup" class="travelo-signup-box travelo-box">
+                    <div class="simple-signup">
+                        <div class="text-center signup-email-section">
+                            <a href="#" class="signup-email"><i class="soap-icon-letter"></i>Sign up with Email</a>
+                        </div>
+                        <p class="description">By signing up, I agree to Travelo's Terms of Service, Privacy Policy, Guest Refund olicy, and Host Guarantee Terms.</p>
+                    </div>
+                    <div>
+                        <form>
+                            <div class="form-group">
+                                <input type="text" class="input-text full-width" placeholder="first name">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="input-text full-width" placeholder="last name">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="input-text full-width" placeholder="email address">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="input-text full-width" placeholder="password">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="input-text full-width" placeholder="confirm password">
+                            </div>
+                            <div class="form-group">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox"> Tell me about Travelo news
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <p class="description">By signing up, I agree to Travelo's Terms of Service, Privacy Policy, Guest Refund Policy, and Host Guarantee Terms.</p>
+                            </div>
+                            <button type="submit" class="full-width btn-medium">SIGNUP</button>
+                        </form>
+                    </div>
+                    <div class="seperator"></div>
+                    <p>Already a Travelo member? <a href="#travelo-login" class="goto-login soap-popupbox">Login</a></p>
+                </div>
+                <div id="travelo-login" class="travelo-login-box travelo-box">
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="input-text full-width" placeholder="email address">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="input-text full-width" placeholder="password">
+                        </div>
+                        <div class="form-group">
+                            <a href="#" class="forgot-password pull-right">Forgot password?</a>
+                            <div class="checkbox checkbox-inline">
+                                <label>
+                                    <input type="checkbox"> Remember me
+                                </label>
+                            </div>
+                        </div>
+                    </form>
+                    <div class="seperator"></div>
+                    <p>Don't have an account? <a href="#travelo-signup" class="goto-signup soap-popupbox">Sign up</a></p>
+                </div>
+            </header>
+    </body>
+</html>
---
+++ includes/jsscripts.php
@@ -0,0 +1,50 @@
+ <!-- Javascript -->
+ <script type="text/javascript" src="assets/js/jquery-1.11.1.min.js"></script>
+        <script type="text/javascript" src="assets/js/jquery.noconflict.js"></script>
+        <script type="text/javascript" src="assets/js/modernizr.2.7.1.min.js"></script>
+        <script type="text/javascript" src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+        <script type="text/javascript" src="assets/js/jquery.placeholder.js"></script>
+        <script type="text/javascript" src="assets/js/jquery-ui.1.10.4.min.js"></script>
+        
+        <!-- Twitter Bootstrap -->
+        <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+        
+        <!-- load revolution slider scripts -->
+        <script type="text/javascript" src="assets/components/revolution_slider/js/jquery.themepunch.plugins.min.js"></script>
+        <script type="text/javascript" src="assets/components/revolution_slider/js/jquery.themepunch.revolution.min.js"></script>
+
+        <!-- Flex Slider -->
+        <script type="text/javascript" src="assets/components/flexslider/jquery.flexslider-min.js"></script>
+        
+        <!-- load BXSlider scripts -->
+        <script type="text/javascript" src="assets/components/jquery.bxslider/jquery.bxslider.min.js"></script>
+        
+        <!-- parallax -->
+        <script type="text/javascript" src="assets/js/jquery.stellar.min.js"></script>
+        
+        <!-- waypoint -->
+        <script type="text/javascript" src="assets/js/waypoints.min.js"></script>
+
+        <!-- load page Javascript -->
+        <script type="text/javascript" src="assets/js/theme-scripts.js"></script>
+        <script type="text/javascript" src="assets/js/scripts.js"></script>
+        
+        <script type="text/javascript">
+            tjq(document).ready(function() {
+                tjq('.revolution-slider').revolution(
+                {
+                    dottedOverlay:"none",
+                    delay:9000,
+                    startwidth:1200,
+                    startheight:646,
+                    onHoverStop:"on",
+                    hideThumbs:10,
+                    fullWidth:"on",
+                    forceFullWidth:"on",
+                    navigationType:"none",
+                    shadow:0,
+                    spinner:"spinner4",
+                    hideTimerBar:"on",
+                });
+            });
+        </script>
