0f324889cff9cd1fddbd21931f734dbc16bae355
ChandrakumaranSubiharan <wordpress.subiharan@gmail.com>
2022-03-16 00:14:26 +0530

    init

---
+++ admin/admin-register.php
@@ -0,0 +1,12 @@
+<form action="" method="post">
+<input type="text" name="afname" class="input-text full-width" placeholder="Enter Full Name">
+<input type="text" name="auname" class="input-text full-width" placeholder="Enter User Name">
+<input type="password" name="apassword" class="input-text full-width" placeholder="Enter New Password">
+<input type="text" name="acontact" class="input-text full-width" placeholder="Enter Contact Number">
+<input type="text" name="astffid" class="input-text full-width" placeholder="Enter Staff ID">
+<input type="text" name="adep" class="input-text full-width" placeholder="Enter Department Name">
+<input type="text" name="amail" class="input-text full-width" placeholder="Enter Email Address">
+<input type="text" name="acategory" class="input-text full-width" placeholder="Select user Category">
+<button type="submit" name="btn-admin-save" class="full-width btn-medium">REGISTER</button>
+
+</form>
