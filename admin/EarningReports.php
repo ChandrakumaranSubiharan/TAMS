e4eed38dc5fe8df6db1211dcc100b2141f2cc1ed
ChandrakumaranSubiharan <wordpress.subiharan@gmail.com>
2022-04-27 22:51:53 +0530

    earning report and booking report updated

---
+++ admin/EarningReports.php
@@ -0,0 +1,371 @@
+<?php
+// Include database file
+include_once '../includes/dbconfig.php';
+
+
+// display record
+if (isset($_GET['type']) && !empty($_GET['type'])) {
+    $Type = $_GET['type'];
+
+    if ($Type == 'DateWise') {
+        $SDate = isset($_POST['Sdate']) ? $_POST['Sdate'] : date("Y-m-d", strtotime(date("Y-m-d") . " -1 week"));
+        $EDate = isset($_POST['Edate']) ? $_POST['Edate'] : date("Y-m-d", strtotime(date("Y-m-d")));
+    } elseif ($Type == 'Daily') {
+        $SDate = isset($_POST['Sdate']) ? $_POST['Sdate'] : date("Y-m-d", strtotime(date("Y-m-d")));
+    }
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Basic Page Info -->
+    <meta charset="utf-8">
+    <title>HappyHolidayss</title>
+
+    <!-- Site favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="../assets/dashboard/vendors/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../assets/dashboard/vendors/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/dashboard/vendors/images/favicon-16x16.png">
+
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="../assets/dashboard/vendors/styles/core.css">
+    <link rel="stylesheet" type="text/css" href="../assets/dashboard/vendors/styles/icon-font.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/dashboard/src/plugins/datatables/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/dashboard/src/plugins/datatables/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/dashboard/vendors/styles/style.css">
+
+    <!-- pdf script -->
+    <script src="../assets/dashboard/vendors/scripts/jspdfmin.js"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119386393-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-119386393-1');
+	</script> -->
+
+
+
+</head>
+
+<body>
+    <?php include('includes/header.php'); ?>
+    <?php include('includes/navigation.php'); ?>
+
+    <div class="mobile-menu-overlay"></div>
+
+
+    <div class="main-container">
+        <div class="pd-ltr-20 xs-pd-20-10">
+            <div class="min-height-200px">
+                <div class="page-header">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+                            <div class="title">
+                                <h4><?php if ($Type == 'DateWise') {
+                                        echo "Date-Wise ";
+                                    } elseif ($Type == 'Daily') {
+                                        echo "Daily ";
+                                    } ?> Earning Reports </h4>
+                            </div>
+                            <nav aria-label="breadcrumb" role="navigation">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Reports</li>
+                                    <li class="breadcrumb-item active" aria-current="page">Earning Category</li>
+
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+
+                <?php
+                // Insert Record in home table
+                if (isset($_POST['submit'])) {
+                    $Rtype = $_POST['ReportType'];
+
+                    if ($Type == 'DateWise') {
+                        $EDate = $_POST['Edate'];
+                    } else {
+                        $EDate = '';
+                    }
+                    $ReportView = $reports->EarningReportAll($Rtype, $SDate, $EDate, $Type);
+                }
+                ?>
+
+                <!-- Form grid Start -->
+                <div class="pd-20 card-box mb-30">
+                    <div class="clearfix">
+                        <div class="pull-left">
+                            <h4 class="text-blue h4">Filter</h4>
+                        </div>
+                    </div>
+                    <form method="POST">
+                        <div class="row">
+                            <div class="col-md-3 col-sm-12">
+                                <div class="form-group">
+                                    <label>Status</label>
+                                    <select class="custom-select2 form-control" name="ReportType" style=" height: 38px;">
+                                        <?php if (isset($_POST['submit'])) {
+                                        ?>
+                                            <option value="<?php if (isset($_POST['submit'])) {
+                                                                echo $Rtype;
+                                                            }; ?>" <?php if (isset($_POST['submit'])) {
+                                                                        echo 'selected="selected"';
+                                                                    }; ?>>
+                                                <?php if (isset($_POST['submit']) && $Rtype == 1) {
+                                                    echo 'Earning Report : Selected';
+                                                } elseif ($Rtype == 2) {
+                                                    echo 'Tour Services Earnings Report : Selected';
+                                                } elseif ($Rtype == 3) {
+                                                    echo 'HomeStay Services Earnings Report : Selected';
+                                                } elseif ($Rtype == 4) {
+                                                    echo 'Earnings Greaterthan 15K Report : Selected';
+                                                } elseif ($Rtype == 5) {
+                                                    echo 'Earnings Lowerthan 15K Report : Selected';
+                                                } elseif ($Rtype == 6) {
+                                                    echo 'Profit Percentage Greaterthan 15% Report : Selected';
+                                                } elseif ($Rtype == 7) {
+                                                    echo 'Profit Percentage Lowerthan 15% Report : Selected';
+                                                }; ?>
+                                            </option>
+                                        <?php
+                                        }
+                                        ?>
+                                        <option value="1">Earning Report</option>
+                                        <option value="2">Tour Services Earnings Report</option>
+                                        <option value="3">HomeStay Services Earnings Report</option>
+                                        <option value="4">Earnings Greaterthan 15K Report</option>
+                                        <option value="5">Earnings Lowerthan 15K Report</option>
+
+                                        <option value="6">Profit Percentage Greaterthan 15% Report</option>
+                                        <option value="7">Profit Percentage Lowerthan 15% Report</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <?php
+
+                            if ($Type == 'Daily') {
+                            ?>
+                                <div class="col-md-4 col-sm-12">
+                                    <div class="form-group">
+                                        <label>Date</label>
+                                        <input class="form-control" placeholder="Select Date" name="Sdate" type="date" value="<?= $SDate; ?>">
+                                    </div>
+                                </div>
+                            <?php
+                            } elseif ($Type == 'DateWise') {
+                            ?>
+                                <div class="col-md-3 col-sm-12">
+                                    <div class="form-group">
+                                        <label>Date From</label>
+                                        <input class="form-control" placeholder="Select Date" name="Sdate" type="date" value="<?= $SDate; ?>">
+                                    </div>
+                                </div>
+                                <div class="col-md-3 col-sm-12">
+                                    <div class="form-group">
+                                        <label>Date To</label>
+                                        <input class="form-control" placeholder="Select Date" name="Edate" type="date" value="<?= $EDate; ?>">
+                                    </div>
+                                </div>
+                            <?php
+                            };
+                            ?>
+
+                            <div class="col-md-3 col-sm-12">
+                                <div class="btn-list" style="display: flex; padding-top: 20px;">
+                                    <button type="submit" name="submit" class="btn btn-primary btn-lg btn-block">FILTER</button>
+                                    <button class="btn btn-success" onclick="printDiv('pdf','Title')">PRINT</button>
+
+                                </div>
+                            </div>
+                        </div>
+
+                </div>
+                </form>
+            </div>
+
+
+            <div id="pdf" class="card-box mb-30">
+
+                <div class="row pd-20">
+                    <style>
+                        #sys_logo {
+                            object-fit: cover;
+                            object-position: center center;
+                            width: 10em;
+                            height: 4em;
+                        }
+                    </style>
+                    <div class="col-2 d-flex justify-content-center align-items-center">
+                        <img src="../assets/images/logo.svg" class="img-circle" id="sys_logo" alt="System Logo">
+                    </div>
+                    <div class="col-8">
+                        <h4 class="text-center"> HappyHolidayss Travel & Accommodation Marketplace </h4>
+                        <h3 class="text-center">
+                            <?php if ($Type == 'Daily') {
+
+                                echo 'Daily';
+                            } else {
+                                echo 'Date-wise';
+                            } ?>
+                            <?php if (isset($_POST['submit'])) {
+
+                                if ($Rtype == 2) {
+                                    echo 'Tour Services';
+                                } elseif ($Rtype == 3) {
+                                    echo 'HomeStay Services';
+                                } elseif ($Rtype == 4) {
+                                    echo 'Earnings Greaterthan 15K';
+                                } elseif ($Rtype == 5) {
+                                    echo 'Earnings Lowerthan 15K';
+                                } elseif ($Rtype == 6) {
+                                    echo 'Profit Percentage Greaterthan 15%';
+                                } elseif ($Rtype == 7) {
+                                    echo 'Profit Percentage Lowerthan 15%';
+                                };
+                            }
+                            ?>
+                            Earning Report</h3>
+                        <h5 class="text-center">as of</h5>
+                        <?php if ($Type == 'Daily') {
+                        ?>
+                            <h5 class="text-center"><?= $SDate ?></h5>
+                        <?php
+                        } else {
+                        ?>
+                            <h5 class="text-center"><?= $SDate ?> - <?= $EDate ?> </h5>
+
+                        <?php
+                        } ?>
+                    </div>
+                    <div class="col-2"></div>
+                </div>
+
+                <!-- Bordered table  start -->
+                <div class="pd-20 card-box mb-30">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">Earning Id</th>
+                                <th scope="col">Booking Id</th>
+                                <th scope="col">Customer</th>
+                                <th scope="col">Service</th>
+                                <th scope="col">Full Amount</th>
+                                <th scope="col">Income</th>
+                                <th scope="col">Income <br> Percentage%</th>
+                                <th scope="col">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            if (isset($_POST['submit'])) {
+
+                                $Reportdata = $reports->EarningReportAll($Rtype, $SDate, $EDate, $Type);
+
+                                $TotalIncome = 0;
+                                if ($Reportdata) {
+                                    foreach ($Reportdata as $Reportdatas) {
+
+                            ?>
+                                        <tr>
+                                            <td><?= $Reportdatas['earning_id']; ?></td>
+                                            <td><?= $Reportdatas['booking_id']; ?></td>
+                                            <td><?= $Reportdatas['first_name'], ' ', $Reportdatas['last_name']; ?></td>
+                                            <td><?= $Reportdatas['service_type']; ?></td>
+                                            <td><?= $Reportdatas['total_amount']; ?> LKR</td>
+                                            <td><?= $Reportdatas['net_amount']; ?> LKR</td>
+                                            <td><?= $Reportdatas['profit_percentage'] ?> %</td>
+                                            <td><?php if ($Reportdatas['payment_status'] == 1) {
+                                                    echo "<span style='color: green;'>Received</span>";
+                                                } elseif ($Reportdatas['payment_status'] >= 2) {
+                                                    echo "<span style='color: blue;'>Refunded</span>";
+                                                } else {
+                                                    echo "<span style='color: red;'>Proccessing</span>";
+                                                }
+                                                ?></td>
+                                            <?php
+                                            if ($Reportdata) {
+                                                $TotalIncome += $Reportdatas['net_amount'];
+                                            }
+                                            ?>
+                                        </tr>
+                                    <?php
+                                    }
+                                    ?>
+                                    <tr>
+                                        <td colspan="10" style="text-align: center; font-weight: bold;">
+                                            <h4> <span style="color: green;">Total Income</span> : LKR
+                                                <?php echo $TotalIncome ?>
+                                            </h4>
+                                        </td>
+                                    </tr>
+                                <?php
+
+
+                                } else {
+                                ?>
+                                    <td colspan="9" style="text-align: center;">No Records Found.</td>
+                            <?php
+
+                                }
+                            }
+                            ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+        <div class="footer-wrap pd-20 mb-20 card-box">
+            HappyHolidayss By <a href="https://github.com/dropways" target="_blank">Chandrakumaran Subiharan</a>
+        </div>
+
+    </div>
+    </div>
+
+    </div>
+
+    <?php include('includes/scripts.php'); ?>
+
+    <script>
+        var doc = new jsPDF();
+
+        function printDiv(divId,
+            title) {
+
+            let mywindow = window.open('', 'PRINT', '"width=1000,height=900,left=300,top=50');
+
+            mywindow.document.write(`<html><head><title>Earning Report</title>`);
+            mywindow.document.write('</head><body >');
+            mywindow.document.write(document.getElementById(divId).innerHTML);
+            mywindow.document.write('</body></html>');
+
+            mywindow.document.close(); // necessary for IE >= 10
+            mywindow.focus(); // necessary for IE >= 10*/
+
+            mywindow.print();
+            mywindow.close();
+
+            return true;
+        }
+    </script>
+</body>
+
+</html>
--- includes/classes/class.reports.php
+++ includes/classes/class.reports.php
@@ -287,4 +287,162 @@ class reports
             return false;
         }
     }
+
+
+    
+    // Booking Report
+
+    public function BookingReportAll($Rtype, $SDate, $EDate, $Type)
+    {
+        $sql = '';
+        $sql = "SELECT DISTINCT tbl_booking.booking_id,tbl_booking.cus_id,tbl_booking.total_amount,tbl_booking.cus_payment_card_type,tbl_booking.start_date,tbl_booking.end_date,tbl_booking.status,tbl_booking.created_date,tbl_booking.payment_status,tbl_booking.total_nights,tbl_booking.total_persons,tbl_booking.total_kids,tbl_booking.total_adults,tbl_booking.payment_card_holder_name,tbl_booking.payment_card_number,tbl_booking.service_id,tbl_booking.service_name,tbl_booking.service_type,tbl_booking.cancellation_ava, tbl_earning.earning_id,tbl_earning.net_amount,tbl_earning.payout,tbl_customer.first_name,tbl_customer.last_name,tbl_customer.email_address,tbl_customer.contact_number
+         from tbl_booking
+         join tbl_customer on tbl_booking.cus_id=tbl_customer.customer_id
+         join tbl_earning on tbl_booking.booking_id=tbl_earning.booking_id
+         where ";
+
+        $TTypeString = 'Tour Package';
+        $HTypeString = 'Home Stay';
+
+        if ($Type == 'DateWise') {
+
+            $SqlCommonQuery = "DATE(tbl_booking.created_date) BETWEEN '$SDate' AND '$EDate'";
+            $SqlOrderByQuery = "ORDER BY created_date DESC";
+
+            if ($Rtype == 1) {
+                $sql .= "$SqlCommonQuery ORDER BY created_date DESC ";
+            } elseif ($Rtype == 2) {
+                $sql .= "$SqlCommonQuery AND tbl_booking.service_type= '$TTypeString' $SqlOrderByQuery ";
+            } elseif ($Rtype == 3) {
+                $sql .= "$SqlCommonQuery AND tbl_booking.service_type= '$HTypeString' $SqlOrderByQuery ";
+            } elseif ($Rtype == 4) {
+                $sql .= "$SqlCommonQuery AND tbl_booking.status= 0 $SqlOrderByQuery ";
+            } elseif ($Rtype == 5) {
+                $sql .= "$SqlCommonQuery AND tbl_booking.status= 2 $SqlOrderByQuery ";
+            } elseif ($Rtype == 6) {
+                $sql .= "$SqlCommonQuery AND tbl_booking.status= 5 $SqlOrderByQuery ";
+            } elseif ($Rtype == 7) {
+                $sql .= "$SqlCommonQuery AND tbl_booking.status= 1 or tbl_booking.status= 3 $SqlOrderByQuery ";
+            } elseif ($Rtype == 8) {
+                $sql .= "$SqlCommonQuery AND tbl_booking.payment_status>=2 $SqlOrderByQuery ";
+            } elseif ($Rtype == 9) {
+                $sql .= "$SqlCommonQuery AND tbl_booking.status= 4 $SqlOrderByQuery ";
+            };
+        } elseif ($Type == 'Daily') {
+
+            $SqlCommonQuery = "DATE(tbl_booking.created_date) = '$SDate'";
+            $SqlOrderByQuery = "ORDER BY created_date DESC";
+
+            if ($Rtype == 1) {
+                $sql .= "$SqlCommonQuery ORDER BY created_date DESC ";
+            } elseif ($Rtype == 2) {
+                $sql .= "$SqlCommonQuery AND tbl_booking.service_type= '$TTypeString' $SqlOrderByQuery ";
+            } elseif ($Rtype == 3) {
+                $sql .= "$SqlCommonQuery AND tbl_booking.service_type= '$HTypeString' $SqlOrderByQuery ";
+            } elseif ($Rtype == 4) {
+                $sql .= "$SqlCommonQuery AND tbl_booking.status= 0 $SqlOrderByQuery ";
+            } elseif ($Rtype == 5) {
+                $sql .= "$SqlCommonQuery AND tbl_booking.status= 2 $SqlOrderByQuery ";
+            } elseif ($Rtype == 6) {
+                $sql .= "$SqlCommonQuery AND tbl_booking.status= 5 $SqlOrderByQuery ";
+            } elseif ($Rtype == 7) {
+                $sql .= "$SqlCommonQuery AND tbl_booking.status= 1 or tbl_booking.status= 3 $SqlOrderByQuery ";
+            } elseif ($Rtype == 8) {
+                $sql .= "$SqlCommonQuery AND tbl_booking.payment_status>=2 $SqlOrderByQuery ";
+            } elseif ($Rtype == 9) {
+                $sql .= "$SqlCommonQuery AND tbl_booking.status= 4 $SqlOrderByQuery ";
+            };
+        }
+
+        $sql .= ";";
+        $query = $this->db->query($sql);
+        $data = array();
+        if ($query->rowCount() > 0) {
+            while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                $data[] = $row;
+            }
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+
+
+
+    // Earning Report
+
+    public function EarningReportAll($Rtype, $SDate, $EDate, $Type)
+    {
+        $sql = '';
+        $sql = "SELECT DISTINCT 
+         tbl_earning.earning_id,tbl_earning.total_amount,tbl_earning.profit_percentage,tbl_earning.net_amount,tbl_earning.payout,tbl_earning.created_date,tbl_earning.service_type,tbl_earning.service_name,
+         tbl_booking.booking_id,tbl_booking.cus_id,tbl_booking.payment_status,tbl_booking.payment_card_holder_name,tbl_booking.payment_card_number,tbl_booking.cus_payment_card_type,
+         tbl_customer.first_name,tbl_customer.last_name,tbl_customer.email_address,tbl_customer.contact_number
+         from tbl_earning
+         join tbl_customer on tbl_earning.customer_id=tbl_customer.customer_id
+         join tbl_booking on tbl_earning.booking_id=tbl_booking.booking_id
+         where ";
+
+        $TTypeString = 'Tour Package';
+        $HTypeString = 'Home Stay';
+        $Amount = 15000;
+        $Percentage = 15;
+
+        if ($Type == 'DateWise') {
+
+            $SqlCommonQuery = "DATE(tbl_earning.created_date) BETWEEN '$SDate' AND '$EDate'";
+            $SqlOrderByQuery = "ORDER BY created_date DESC";
+
+            if ($Rtype == 1) {
+                $sql .= "$SqlCommonQuery ORDER BY created_date DESC ";
+            } elseif ($Rtype == 2) {
+                $sql .= "$SqlCommonQuery AND tbl_earning.service_type= '$TTypeString' $SqlOrderByQuery ";
+            } elseif ($Rtype == 3) {
+                $sql .= "$SqlCommonQuery AND tbl_earning.service_type= '$HTypeString' $SqlOrderByQuery ";
+            } elseif ($Rtype == 4) {
+                $sql .= "$SqlCommonQuery AND tbl_earning.total_amount>= '$Amount' $SqlOrderByQuery ";
+            } elseif ($Rtype == 5) {
+                $sql .= "$SqlCommonQuery AND tbl_earning.total_amount<= '$Amount' $SqlOrderByQuery ";
+            } elseif ($Rtype == 6) {
+                $sql .= "$SqlCommonQuery AND tbl_earning.profit_percentage>= '$Percentage' $SqlOrderByQuery ";
+            } elseif ($Rtype == 7) {
+                $sql .= "$SqlCommonQuery AND tbl_earning.profit_percentage<= '$Percentage' $SqlOrderByQuery ";
+            };
+        } elseif ($Type == 'Daily') {
+
+            $SqlCommonQuery = "DATE(tbl_earning.created_date) = '$SDate'";
+            $SqlOrderByQuery = "ORDER BY created_date DESC";
+
+            if ($Rtype == 1) {
+                $sql .= "$SqlCommonQuery ORDER BY created_date DESC ";
+            } elseif ($Rtype == 2) {
+                $sql .= "$SqlCommonQuery AND tbl_earning.service_type= '$TTypeString' $SqlOrderByQuery ";
+            } elseif ($Rtype == 3) {
+                $sql .= "$SqlCommonQuery AND tbl_earning.service_type= '$HTypeString' $SqlOrderByQuery ";
+            } elseif ($Rtype == 4) {
+                $sql .= "$SqlCommonQuery AND tbl_earning.total_amount>= '$Amount' $SqlOrderByQuery ";
+            } elseif ($Rtype == 5) {
+                $sql .= "$SqlCommonQuery AND tbl_earning.total_amount<= '$Amount' $SqlOrderByQuery ";
+            } elseif ($Rtype == 6) {
+                $sql .= "$SqlCommonQuery AND tbl_earning.profit_percentage>= '$Percentage' $SqlOrderByQuery ";
+            } elseif ($Rtype == 7) {
+                $sql .= "$SqlCommonQuery AND tbl_earning.profit_percentage<= '$Percentage' $SqlOrderByQuery ";
+            };
+        }
+
+        $sql .= ";";
+        $query = $this->db->query($sql);
+        $data = array();
+        if ($query->rowCount() > 0) {
+            while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                $data[] = $row;
+            }
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+
 }
