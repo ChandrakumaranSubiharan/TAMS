9c41370bbf08f89541a9ae7341eade9bb0ce857f
ChandrakumaranSubiharan <wordpress.subiharan@gmail.com>
2022-05-18 01:49:34 +0530

    updated

--- DB/tams_db.sql
+++ DB/tams_db.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2022 at 08:28 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 7.4.27
+-- Generation Time: May 17, 2022 at 10:00 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -78,16 +79,6 @@ CREATE TABLE `tbl_booking` (
   `cancellation_ava` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tbl_booking`
---
-
-INSERT INTO `tbl_booking` (`booking_id`, `total_amount`, `cus_payment_card_type`, `cus_id`, `start_date`, `end_date`, `status`, `created_date`, `updated_date`, `payment_status`, `total_nights`, `total_persons`, `partner_id`, `total_kids`, `total_adults`, `service_id`, `service_name`, `service_type`, `payment_card_holder_name`, `payment_card_number`, `cancellation_ava`) VALUES
-(1112, 5000, 'Master Card', 3030, '2022-04-17', '2022-04-21', 4, '2022-04-19 22:00:00', '2022-04-29 20:05:57', 1, 1, 3, 6061, 1, 2, 4044, 'Madulkelle Tea and Eco Lodge', 'Home Stay', 'fsd', 24124, 0),
-(101011, 2500, 'Visa Card', 3030, '2022-04-29', '2022-04-30', 4, '2022-04-23 09:57:00', '2022-04-30 19:13:50', 1, 1, 1, 6061, 0, 1, 4044, 'Madulkelle Tea and Eco Lodge', 'Home Stay', 'dsfds', 123213, 1),
-(101013, 5000, 'Master Card', 3030, '2022-04-25', '2022-05-05', 5, '2022-04-25 16:00:48', '2022-05-02 11:17:03', 1, 9, 1, 6061, 0, 1, 8, 'Sri Lanka Uncovered', 'Tour Package', 'asdas', 12421, 0),
-(101015, 30000, 'Master Card', 3030, '2022-05-15', '2022-05-18', 4, '2022-05-02 09:30:12', '2022-05-02 11:15:58', 1, 3, 4, 6061, 2, 2, 404041, 'The Kandy House', 'Home Stay', 'sfasf', 14214, 0);
-
 -- --------------------------------------------------------
 
 --
@@ -115,8 +106,14 @@ CREATE TABLE `tbl_customer` (
 INSERT INTO `tbl_customer` (`customer_id`, `first_name`, `last_name`, `address`, `email_address`, `contact_number`, `username`, `password`, `created_date`, `updated_date`, `status`) VALUES
 (37, 'admin', 'admin', 'admin', 'admin@xmail.com', '14124124', 'adminsanju', '$2y$10$4aSwPX.ZZHbd8MBUcEZnfeNDTH4ofW3hgzgMe7swfqXwP4X4Isw1C', '2022-03-15 07:37:29', '2022-04-19 21:12:35', 1),
 (3030, 'Subiharan', 'Chandrakumaran', '12/3, pothukola, panwila, kandy', 'Subiharan@gmail.com', '0765430351', 'user', '$2y$10$/nsDkanR7e6sPRxgnvXVJeZkOmdbduRJdVCdyQnvMQzeFgyjqNTbq', '2022-04-20 02:22:05', '2022-05-02 09:33:18', 1),
-(303030, 'manoj', 'ragal', '45,wattegama, kandy', 'manoj@gmail.com', '07765484621', 'manoj', '$2y$10$ZdFcts4G0wtw1AhsAT.HueHoCHsS3LvsnwdTyvxp5QDNpa6DV.ilW', '2022-04-29 20:56:57', NULL, 1),
-(303031, 'dilshan', 'keel', 'dilshan@gmail.com', 'dilshan@hotmail.com', '0775530351', 'dilshan', '$2y$10$gOUstja1IoUZaiif//KEVe4f.HTZyF8JUbYY9sbt7b7yJC/Hr31x.', '2022-04-29 20:57:45', NULL, 1);
+(303030, 'Manoj', 'Ragal', '45,wattegama, kandy', 'manoj@gmail.com', '07765484621', 'manoj', '$2y$10$ZdFcts4G0wtw1AhsAT.HueHoCHsS3LvsnwdTyvxp5QDNpa6DV.ilW', '2022-04-29 20:56:57', '2022-05-17 10:17:41', 1),
+(303031, 'Dilshan', 'Keel', 'dilshan@gmail.com', 'dilshan@hotmail.com', '0775530351', 'dilshan', '$2y$10$gOUstja1IoUZaiif//KEVe4f.HTZyF8JUbYY9sbt7b7yJC/Hr31x.', '2022-04-29 20:57:45', '2022-05-17 10:17:26', 1),
+(303032, 'Tonya', 'Barnett', '3372 E Pecan St, Kandy', 'tonya.barnett@gmail.com', '0776306246', 'tonya', '$2y$10$YqJQQeWJp32eNn/4KvziY.V3l22ehj.Uzt0hAgGXw3yN2oAEOMtFa', '2022-05-17 10:04:13', NULL, 1),
+(303033, 'Theresa', 'Scott', '2017 W Pecan St, colombo', 'theresa.scott@gmail.com', '0770257420', 'theresa', '$2y$10$wRv.FHCedgRcIxak4U7M/OLrwqAmXZ31oklOdwGo1P6SuasKnoumm', '2022-05-17 10:09:17', NULL, 1),
+(303034, 'Marian ', 'Hamilton', '6046 Hickory Creek Dr, gampha', 'marian.hamilton@gmail.com', '0778644442', 'marian', '$2y$10$Q0xc5fXOBOv1HPQkzh4l9erJ1l7xhVrliYnkY310YZv63/PgCb3G2', '2022-05-17 10:13:00', NULL, 1),
+(303035, 'Miguel', 'Gilbert', '6034 James St, colombo', 'miguel.gilbert@gmail.com', '0770705789', 'miguel', '$2y$10$oJFHbpp.iOe2n2qODo0L3.08gqlFteJshcL3gG9fCWsX6vVaoUkRm', '2022-05-17 10:14:33', NULL, 1),
+(303036, 'Herminia', 'Hernandez', '9218 E Center St, kandy', 'herminia.hernandez@yahoo.com', '0779720525', 'herminia', '$2y$10$TxdsWM/g1KBHU0ZkfNFudu9teJyYqovUJ1TDQcxj0DrRADw8OWWkq', '2022-05-17 10:15:58', NULL, 1),
+(303037, 'Albert', 'Woods', '1700 Oak Lawn Ave, kandy', 'albert.woods@gmail.com', '0778947735', 'albert', '$2y$10$HTqmGs.gclAGjPxZ0gSRTeBMpN18/DAsxL5Liclsd9ebOgQgISPLm', '2022-05-17 10:17:06', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -137,20 +134,6 @@ CREATE TABLE `tbl_earning` (
   `service_name` varchar(100) DEFAULT NULL,
   `profit_percentage` decimal(10,0) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tbl_earning`
---
-
-INSERT INTO `tbl_earning` (`earning_id`, `booking_id`, `total_amount`, `payout`, `net_amount`, `customer_id`, `created_date`, `service_type`, `service_id`, `service_name`, `profit_percentage`) VALUES
-(71, 1110, 15000, 13500, 1500, 3030, '2022-04-20 15:04:53', 'Tour Package', 8, 'Sri Lanka Uncovered', '10'),
-(72, 1111, 15000, 13500, 1500, 3030, '2022-04-20 15:05:49', 'Home Stay', 4044, 'Madulkelle Tea and Eco Lodge', '10'),
-(73, 1112, 6500, 5850, 650, 3030, '2022-04-20 15:16:38', 'Home Stay', 4044, 'Madulkelle Tea and Eco Lodge', '20'),
-(808080, 101011, 2500, 2250, 250, 3030, '2022-04-23 09:57:00', 'Home Stay', 4044, 'Madulkelle Tea and Eco Lodge', '10'),
-(808081, 101012, NULL, 5000, NULL, 0, '2022-04-25 11:41:27', NULL, NULL, NULL, NULL),
-(808082, 101013, 5000, 4500, 500, 3030, '2022-04-25 16:00:48', 'Tour Package', 8, 'Sri Lanka Uncovered', '10'),
-(808083, 101014, 123, 110.7, 12.3, 3030, '2022-04-27 13:02:12', 'Tour Package', 505051, 'sad', '10'),
-(808084, 101015, 30000, 24000, 6000, 3030, '2022-05-02 09:30:13', 'Home Stay', 404041, 'The Kandy House', '20');
 
 -- --------------------------------------------------------
 
@@ -248,7 +231,15 @@ CREATE TABLE `tbl_partner` (
 
 INSERT INTO `tbl_partner` (`partner_id`, `first_name`, `last_name`, `username`, `password`, `address`, `email_address`, `contact_number`, `status`, `gender`, `zipcode`, `created_date`, `updated_date`, `province`) VALUES
 (6061, 'subiharan', 'chandrakumaran', 'subiharan', '$2y$10$uiab1iacEH45hkBiB63f1OLxprsM4qMJRawGvX8XyRSIShgLagsRy', '56,panwila,kandy', 'partner@gmail.com', '0775430351', 1, 'Male', 213324, '2022-04-20 06:43:59', '2022-04-30 19:25:04', 'Central'),
-(606062, 'partner2', 'partner2', 'partner2', '$2y$10$aVWLtQN3TRjgNRStJDA79OJ/t.Dr7eeP0sbfr.SDu.0/Uf3pEzExS', 'asdasd', 'partner2@gmail.com', '14214', 1, 'male', 1221, '2022-04-27 12:37:19', '2022-04-27 12:37:53', 'Eastern');
+(606062, 'Carolyn', 'Hoffman', 'carolyn', '$2y$10$aVWLtQN3TRjgNRStJDA79OJ/t.Dr7eeP0sbfr.SDu.0/Uf3pEzExS', '4433 Samaritan Dr, Kandy, Sri Lanka', 'caroly@gmail.com', '0784364278', 0, 'male', 122131, '2022-04-27 12:37:19', '2022-05-16 20:42:54', 'Central'),
+(606063, 'Melvin', 'Jensen', 'melvin', '$2y$10$4BQriPY4MXBddofIES012use9frxvK9b1Lvwl4nwFEPxhAvduXBFK', '8823 Plum St, Kandy, Sri Lanka', 'melvin.jensen@gmail.com', '0767213902', 1, 'male', 654365, '2022-05-16 20:07:23', '2022-05-17 10:21:00', 'Central'),
+(606064, 'Marcus ', 'Thomas', 'marcus ', '$2y$10$MkvO.6w6cePE5nWiTsKqnOq.aoGrxtNT3prjI0z4wN4ybyCZvT4nC', '8100 College St, Colombo, Sri lanka', 'marcus.thomas@gmail.com', '0771046307', 0, 'male', 983561, '2022-05-16 20:10:42', NULL, 'Western'),
+(606065, 'Kaylee ', 'Nichols', 'kaylee', '$2y$10$HW5PuXV0NvxZzungxz9sxe4xItv35PawgbZR/pQAPe40oHT1YR.lu', '3096 Marsh Ln, Vavuniya, Sri Lanka', 'kaylee.nichols@yahoo.com', '0787570125', 0, 'female', 987452, '2022-05-16 20:12:47', NULL, 'Northern'),
+(606066, 'Lauren', 'Edwards', 'lauren', '$2y$10$ejKnsJ1.CpvLoGbTLoMXQ.ZcczyWcwKUshxehKMoXCTNy/vfqAUGO', '5797 Poplar Dr, Jaffna, Sri Lanka', 'lauren.edwards@gmail.com', '0777712350', 0, 'female', 765321, '2022-05-16 20:16:01', NULL, 'Northern'),
+(606067, 'Frank ', 'Hanson', 'frank', '$2y$10$1dqnXseDfqgVVEkNwGr.9uu8tW5s1mm66utNKVhgQY6DyHpk9gPHy', '8885 Spring Hill Rd, Galle, Sri Lanka', 'frank.hanson@gmail.com', '0714837288', 0, 'male', 654234, '2022-05-16 20:20:44', NULL, 'Southern'),
+(606068, 'Jordan ', 'Long', 'jordan', '$2y$10$QQ.QwVz1SUutNvfXBinkReOiKuPAzXFlzoi4umR9paQXZ439ovyde', '7831 Adams St, batticaloa, sri lanka', 'jordan.long@gmail.com', '0762107539', 0, 'male', 566797, '2022-05-16 20:32:06', NULL, 'Eastern'),
+(606069, 'Clara', 'Castro', 'clara', '$2y$10$b5b4zFV3e/cgQM0TF5oZr.GpLO/9gu8odSdzLrz.rG3lcYE9cBlXm', '6058 Mcclellan Rd, Matale, Sri Lanka', 'clara.castro@gmail.com', '0717120093', 0, 'female', 574432, '2022-05-16 20:36:20', NULL, 'Central'),
+(606070, 'Phillip', 'Griffin', 'phillip', '$2y$10$CzdWw458XnLb/2QJf/9p/OX7ETkjIke.rr3rsrKEGvolQ0p/ZJeAm', '9350 Wheeler Ridge Dr, Gampaha, Sri Lanka', 'phillip.griffin@gmail.com', '0785181473', 0, 'male', 564634, '2022-05-16 20:40:58', NULL, 'Western');
 
 -- --------------------------------------------------------
 
@@ -316,13 +307,16 @@ CREATE TABLE `tbl_tour` (
 --
 
 INSERT INTO `tbl_tour` (`tour_id`, `title`, `details`, `location`, `tour_type`, `duration_nights`, `adult_price`, `image`, `created_date`, `updated_date`, `partner_id`, `status`, `language`, `availabile_seats`, `ava_start_date`, `ava_end_date`, `cancellation`, `district`, `gathering_location`, `s_time`, `e_time`, `kid_price`, `kid_status`) VALUES
-(9, 'Sri Lanka Uncovered', 'Start in Negombo and end in Unawatuna! With the Active Adventure tour Sri Lanka Uncovered, you have a 10 days tour package taking you through Negombo, Sri Lanka and 7 other destinations in Sri Lanka. Sri Lanka Uncovered includes accommodation, an expert guide, meals, transport and more. ', 'Negombo', 'Active Adventure', 9, 5000, '2048692867.jpg', '2022-04-20 15:02:41', '2022-04-29 20:41:21', 6061, 0, 'English', 45, '2022-04-25', '2022-05-05', 0, 'Colombo', 'near negombo bus stand', '08:00:00', '17:00:00', 2500, 1),
+(9, 'Sri Lanka Uncovered', 'Start in Negombo and end in Unawatuna! With the Active Adventure tour Sri Lanka Uncovered, you have a 10 days tour package taking you through Negombo, Sri Lanka and 7 other destinations in Sri Lanka. Sri Lanka Uncovered includes accommodation, an expert guide, meals, transport and more. ', 'Negombo', 'Active Adventure', 9, 5000, '2048692867.jpg', '2022-04-20 15:02:41', '2022-05-17 10:31:45', 6061, 1, 'English', 45, '2022-05-25', '2022-06-04', 0, 'Colombo', 'near negombo bus stand', '08:00:00', '17:00:00', 2500, 1),
 (505051, 'sad', 'asfasf', 'asda', 'Active Adventure', 3, 123, '1030306262.jpg', '2022-04-27 12:39:02', '2022-04-29 19:54:54', 606062, 1, 'Tamil', 1, '2022-04-28', '2022-04-30', 1, 'Colombo', 'ASFASF', '05:55:00', '04:44:00', 0, 0),
-(505052, 'Sacred Sri Lanka', 'Start and end in Colombo! With the In-depth Cultural tour Sacred Sri Lanka - Free Upgrade to Private Tour Available, you have a 5 days tour package taking you through Colombo, Sri Lanka and 6 other destinations in Sri Lanka. Sacred Sri Lanka - Free Upgrade to Private Tour Available includes accommodation in a hotel as well as an expert guide, meals, transport and more. ', 'Colombo', 'In-depth Cultural', 9, 15000, '103122_28bd5386.jpg', '2022-04-29 16:31:36', '2022-04-29 16:55:53', 6061, 1, 'english', 30, '2022-05-10', '2022-05-20', 1, 'Colombo', 'Near Colombo Railway Station', '08:00:00', '08:00:00', 0, 0),
-(505053, 'Amazing Sri Lanka', 'Start in Kandy and end in Colombo! With the Active Adventure tour Amazing Sri Lanka, you have a 5 days tour package taking you through Kandy, Sri Lanka and 6 other destinations in Sri Lanka. Amazing Sri Lanka includes accommodation in a hotel as well as an expert guide, meals, transport and more. ', 'Kandy', 'Explorer', 4, 10000, '175872_7ada2a64.jpg', '2022-04-29 16:33:33', '2022-04-29 16:58:44', 6061, 1, 'english', 50, '2022-05-10', '2022-05-15', 1, 'Kandy', 'Kandy Thalatha Maligawa', '08:00:00', '18:00:00', 5000, 1),
-(505054, 'Wonders of Sri Lanka', 'Start in Negombo and end in Colombo! With the Active Adventure tour Sri Lanka\'s Bestseller - Wonders of Sri Lanka, you have a 6 days tour package taking you through Negombo, Sri Lanka and 8 other destinations in Sri Lanka. Sri Lanka\'s Bestseller - Wonders of Sri Lanka includes accommodation in a hotel as well as an expert guide, meals, transport and more. ', 'Negombo', 'Active Adventure', 5, 20000, '176045_479ea4f8.jpg', '2022-04-29 16:40:55', '2022-04-29 16:43:27', 6061, 1, 'English', 50, '2022-05-15', '2022-05-20', 0, 'Kandy', 'near negombo main bus stand', '08:00:00', '20:00:00', 10000, 1),
+(505052, 'Sacred Sri Lanka', 'Start and end in Colombo! With the In-depth Cultural tour Sacred Sri Lanka - Free Upgrade to Private Tour Available, you have a 5 days tour package taking you through Colombo, Sri Lanka and 6 other destinations in Sri Lanka. Sacred Sri Lanka - Free Upgrade to Private Tour Available includes accommodation in a hotel as well as an expert guide, meals, transport and more. ', 'Colombo', 'In-depth Cultural', 9, 15000, '103122_28bd5386.jpg', '2022-04-29 16:31:36', '2022-05-17 10:35:16', 6061, 1, 'english', 30, '2022-05-25', '2022-06-05', 1, 'Colombo', 'Near Colombo Railway Station', '08:00:00', '08:00:00', NULL, 0),
+(505053, 'Amazing Sri Lanka', 'Start in Kandy and end in Colombo! With the Active Adventure tour Amazing Sri Lanka, you have a 5 days tour package taking you through Kandy, Sri Lanka and 6 other destinations in Sri Lanka. Amazing Sri Lanka includes accommodation in a hotel as well as an expert guide, meals, transport and more. ', 'Kandy', 'Explorer', 4, 10000, '175872_7ada2a64.jpg', '2022-04-29 16:33:33', '2022-05-17 10:40:31', 6061, 1, 'english', 50, '2022-05-22', '2022-05-27', 1, 'Kandy', 'Kandy Thalatha Maligawa', '08:00:00', '18:00:00', 5000, 1),
+(505054, 'Wonders of Sri Lanka', 'Start in Negombo and end in Colombo! With the Active Adventure tour Sri Lanka\'s Bestseller - Wonders of Sri Lanka, you have a 6 days tour package taking you through Negombo, Sri Lanka and 8 other destinations in Sri Lanka. Sri Lanka\'s Bestseller - Wonders of Sri Lanka includes accommodation in a hotel as well as an expert guide, meals, transport and more. ', 'Negombo', 'Active Adventure', 5, 20000, '176045_479ea4f8.jpg', '2022-04-29 16:40:55', '2022-05-17 10:33:58', 6061, 1, 'English', 50, '2022-05-24', '2022-05-30', 0, 'Kandy', 'near negombo main bus stand', '08:00:00', '20:00:00', 10000, 1),
 (505055, 'Splendour Of Sri Lanka', 'Start in Colombo and end in Negombo! With the In-depth Cultural tour Splendour Of Sri Lanka (7 Days) Free Upgrade to Private Tour Available, you have a 7 days tour package taking you through Colombo, Sri Lanka and 10 other destinations in Sri Lanka. Splendour Of Sri Lanka (7 Days) Free Upgrade to Private Tour Available includes accommodation in a hotel as well as an expert guide, meals, transport and more.', 'Colombo', 'In-depth Cultural', 6, 20000, '775773148.jpg', '2022-04-29 17:31:20', '2022-04-29 19:55:26', 6061, 1, 'English', 50, '2022-05-20', '2022-07-27', 1, 'Colombo', 'Colombo Main Bus Stand', '06:00:00', '08:00:00', 10000, 1),
-(505056, 'Highlights of Sri Lanka', 'Start and end in Negombo! With the In-depth Cultural tour Highlights of Sri Lanka, you have a 8 days tour package taking you through Negombo, Sri Lanka and 10 other destinations in Sri Lanka. Highlights of Sri Lanka includes accommodation in a hotel as well as an expert guide, meals, transport and more.', 'Batticaloa', 'In-depth Cultural', 7, 10000, '261213411.jpg', '2022-04-30 19:17:47', '2022-04-30 19:19:29', 6061, NULL, 'English', 45, '2022-05-14', '2022-05-22', 1, 'Choose...', 'batticaloa bus stand', '08:00:00', '08:00:00', 5000, 1);
+(505056, 'Highlights of Sri Lanka', 'Start and end in Negombo! With the In-depth Cultural tour Highlights of Sri Lanka, you have a 8 days tour package taking you through Negombo, Sri Lanka and 10 other destinations in Sri Lanka. Highlights of Sri Lanka includes accommodation in a hotel as well as an expert guide, meals, transport and more.', 'Batticaloa', 'In-depth Cultural', 7, 10000, '261213411.jpg', '2022-04-30 19:17:47', '2022-05-17 10:34:34', 6061, 1, 'English', 45, '2022-05-25', '2022-06-02', 1, 'Batticaloa', 'batticaloa bus stand', '08:00:00', '08:00:00', 5000, 1),
+(505057, 'UNESCO World heritage adventure tour', 'Start and end in Colombo! With the Hiking & Trekking tour 6-Day Central Highlands UNESCO World heritage adventure tour, you have a 6 days tour package taking you through Colombo, Sri Lanka and 3 other destinations in Sri Lanka. 6-Day Central Highlands UNESCO World heritage adventure tour includes accommodation in a hotel as well as an expert guide, meals, transport and more. ', 'Colombo', 'Hiking & Trekking', 5, 30000, '330190387.jpg', '2022-05-17 10:45:42', '2022-05-17 10:46:23', 6061, 1, 'English', 45, '2022-05-22', '2022-05-28', 1, 'Nuwara Eliya', 'Near Fort Railway Station', '08:00:00', '18:00:00', NULL, 0),
+(505058, 'Wild About Sri Lanka', 'Start and end in Colombo! With the Wildlife tour Wild About Sri Lanka - 10 days, you have a 10 days tour package taking you through Colombo, Sri Lanka and 9 other destinations in Sri Lanka. Wild About Sri Lanka - 10 days includes accommodation in a hotel as well as an expert guide, meals, transport and more.', 'Colombo', 'Wildlife', 9, 45000, '1688613207.jpg', '2022-05-17 10:51:03', '2022-05-17 10:51:21', 6061, 1, 'English', 25, '2022-05-31', '2022-06-10', 0, 'Badulla', 'Near Fort Railway Station', '08:00:00', '18:00:00', 15000, 1),
+(505059, 'Grand Adventure Tour', 'Start and end in Kandy! With the Explorer tour Grand Adventure Tour Sri Lanka, you have a 13 days tour package taking you through Kandy, Sri Lanka and 17 other destinations in Sri Lanka. Grand Adventure Tour Sri Lanka includes accommodation in a hotel as well as an expert guide, meals, transport and more. ', 'Kandy', 'Active Adventure', 14, 50000, '1313022199.jpg', '2022-05-17 10:54:28', '2022-05-17 10:54:42', 6061, 1, 'English', 45, '2022-06-01', '2022-06-15', 0, 'Kandy', 'Near Kandy Railway Station', '08:00:00', '18:00:00', 20000, 1);
 
 --
 -- Indexes for dumped tables
@@ -403,7 +397,7 @@ ALTER TABLE `tbl_booking`
 -- AUTO_INCREMENT for table `tbl_customer`
 --
 ALTER TABLE `tbl_customer`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=303032;
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=303038;
 
 --
 -- AUTO_INCREMENT for table `tbl_earning`
@@ -427,7 +421,7 @@ ALTER TABLE `tbl_inquiry`
 -- AUTO_INCREMENT for table `tbl_partner`
 --
 ALTER TABLE `tbl_partner`
-  MODIFY `partner_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=606063;
+  MODIFY `partner_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=606071;
 
 --
 -- AUTO_INCREMENT for table `tbl_review`
@@ -439,7 +433,7 @@ ALTER TABLE `tbl_review`
 -- AUTO_INCREMENT for table `tbl_tour`
 --
 ALTER TABLE `tbl_tour`
-  MODIFY `tour_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=505057;
+  MODIFY `tour_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=505060;
 
 --
 -- Constraints for dumped tables
